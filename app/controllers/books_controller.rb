0f0010ef7d94a2e2158a537ea12843980fb88c18
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-03-23 10:18:31 +0000

    [User機能追加]課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   def create
     @book=Book.new(book_params)
     @book.user_id = current_user.id
@@ -10,16 +10,17 @@ class BooksController < ApplicationController
   def index
     @book=Book.new
     @books=Book.all
+    @user=current_user.id
   end
-  
+
   def show
     @book=Book.find(params[:id])
   end
-  
+
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def destroy
     book=Book.find(params[:id])
     book.destroy
---
+++ db/migrate/20230323055458_addintroduction_to_users.rb
@@ -0,0 +1,6 @@
+class AddintroductionToUsers < ActiveRecord::Migration[6.1]
+  def change
+  end
+  
+  
+end
